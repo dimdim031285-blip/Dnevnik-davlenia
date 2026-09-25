009aff1299ddf1c3c6bf975f643b3e0ad081f7d1
dimdim031285-blip <dimdim031285@gmail.com>
2026-09-25 09:18:43 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,5 @@
-const CACHE='bp-diary-v3';
+const CACHE='bp-diary-v4';
 const ASSETS=['./','./index.html','./manifest.json'];
-self.addEventListener('install',event=>{
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(
-    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
-  )).then(()=>self.clients.claim()));
-});
-self.addEventListener('fetch',event=>{
-  event.respondWith(
-    caches.match(event.request).then(r=>r||fetch(event.request))
-  );
-});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
